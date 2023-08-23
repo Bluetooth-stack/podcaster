905849d999a15babbc59cb7352e7e32f75814a6c
Siddharth Hota <siddharthhota26@gmail.com>
2023-08-23 21:55:45 +0530

    transition

--- src/PageTransition.js
+++ src/PageTransition.js
@@ -7,7 +7,7 @@ const PageTransition = ({ children }) => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.6, ease: 'linear' }}
+            transition={{ duration: 0.36, ease: 'linear' }}
         >
             {children}
         </motion.div>
